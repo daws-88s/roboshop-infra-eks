a8cbd5f76679d0f5b71c131a3f12e4e6f3c5fa8b
Sivakumar Reddy M <info@joindevops.com>
2026-04-21 09:00:59 +0530

    k8

--- 70-acm/provider.tf
+++ 70-acm/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket  = "remote-state-aws-88s-dev" # Replace with your unique bucket name
-    key     = "roboshop-dev-acm"
+    key     = "terraform-eks-acm"
     region  = "us-east-1"
     encrypt = true
     use_lockfile   = true
--- 80-frontend-alb/provider.tf
+++ 80-frontend-alb/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket  = "remote-state-aws-88s-dev" # Replace with your unique bucket name
-    key     = "roboshop-dev-frontend-alb"
+    key     = "terraform-eks-alb"
     region  = "us-east-1"
     encrypt = true
     use_lockfile   = true
